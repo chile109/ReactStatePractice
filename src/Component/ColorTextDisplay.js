7af12ba00a5b04389df34e74323a07439e5165d1
kevinshark <chile109@gmail.com>
2024-06-13 18:46:17 +0800

    6th: useEffect to log changes

--- src/Component/ColorTextDisplay.js
+++ src/Component/ColorTextDisplay.js
@@ -1,8 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { ColorTextContext } from "../Context/ColorTextContext";
 
 function ColorTextDisplay() {
     const { color, text, colorClicks, textClicks } = useContext(ColorTextContext);
+
+    useEffect(() => {
+        console.log(`Color changed to: ${color}`);
+    }, [color]);
+
+    useEffect(() => {
+        console.log(`Text changed to: ${text}`);
+    }, [text]);
+
     return (
         <div> <h1 style={{ color }}>{text}</h1>
             <p>Color change clicks: {colorClicks}</p>
